b21d3752d85accfc7379ba42e23cd24718438467
Jeremy Woertink <jeremywoertink@gmail.com>
2015-01-26 13:04:19 -0800

    param removed

--- app/models/appointment.rb
+++ app/models/appointment.rb
@@ -8,7 +8,7 @@ class Appointment < ActiveRecord::Base
   def self.check_in(visitor, params)
     directory = Directory.by_name('Lobby')
     appointment = visitor.appointment
-    appointment_params = {directory_id: directory.id, appointment_visitor_attributes: params.merge(checked_in_at: Time.current, appointment_id: appointment.id)}
+    appointment_params = {directory_id: directory.id, appointment_visitor_attributes: params.merge(checked_in_at: Time.current)}
     appointment.update_attributes(appointment_params) ? appointment : nil
   end
 
